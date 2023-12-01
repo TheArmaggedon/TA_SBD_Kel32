7b067226b954ae02f929aae8b67a391ab873e979
Muhammad Farrel Asyraf Abrar <farrelabrar@gmail.com>
2023-12-01 18:59:50 +0700

    modified recyclebin controller

--- app/Http/Controllers/RecyclebinController.php
+++ app/Http/Controllers/RecyclebinController.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\DB;
 class RecyclebinController extends Controller
 {
     public function index() {
-        $dataKaryawan = DB::select('SELECT * from karyawan where terhapus is TRUE');
-        $dataKartuAkses = DB::select('SELECT * from kartu_akses where terhapus is TRUE');
-        $dataRuangan = DB::select('SELECT * from ruangan where terhapus is TRUE');
+        $dataKaryawan = DB::select('SELECT * from karyawan where terhapus = 1');
+        $dataKartuAkses = DB::select('SELECT * from kartu_akses where terhapus = 1');
+        $dataRuangan = DB::select('SELECT * from ruangan where terhapus = 1');
         return view('recyclebin.index', [
             'dataKaryawan' => $dataKaryawan, 
             'dataKartuAkses' => $dataKartuAkses, 
